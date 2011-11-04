99fcc95aaa979ddea7f93d14d186e8002e758e7e
asonas <hzw1258@gmail.com>
2011-11-05 03:59:14 +0900

    バッチ修正

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -25,4 +25,23 @@ class CardsController < ApplicationController
       format.xml  { render :xml => @card }
     end
   end
+  
+  def batch
+  search = Twitter::Search.new
+search.containing("#津山弥生祭").language("ja").result_type("recent").each do |r|
+  #puts r
+  p r.id
+  db = SQLite3::Database.new("/home/asonas/www/gekibun/db/development.sqlite3")
+  hoge = db.execute('select * from cards where tweet_id = ?', r.id)
+  db.close
+  p hoge
+  if hoge.empty?
+    insdb = SQLite3::Database.new("/home/asonas/www/gekibun/db/development.sqlite3")
+    new_item = {:message => r.text, :tweet_id => r.id}
+    sql = "insert into cards values (:id, :message, :tweet_id, :created_at, :updated_at)"
+    insdb.execute(sql, new_item)
+    insdb.close
+  end
+end
+  end
 end
