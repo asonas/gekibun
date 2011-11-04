a3e20b3e40a2983ba3f2b426bdfff850057dd4ed
asonas <hzw1258@gmail.com>
2011-11-05 03:02:13 +0900

    バッチ書いた

--- script/batch.rb
+++ script/batch.rb
@@ -1,8 +1,22 @@
 # -*- coding: utf-8 -*-
 require 'twitter'
-search = Twitter::Search.new
+require 'sqlite3'
 
-search.containing("#津山弥生祭").language("ja").result_type("recent").per_page(5).each do |r|
-puts r
-  p "#{r.from_user}:#{r.text}"
+search = Twitter::Search.new
+search.containing("#津山弥生祭").language("ja").result_type("recent").each do |r|
+  #puts r
+  p r.id
+  db = SQLite3::Database.new("../db/development.sqlite3")
+  hoge = db.execute('select * from cards where tweet_id = ?', r.id)
+  db.close
+  p hoge
+  if hoge.empty?
+    p 'haittenai'
+    insdb = SQLite3::Database.new("../db/development.sqlite3")
+    new_item = {:message => r.text, :tweet_id => r.id}
+    sql = "insert into cards values (:id, :message, :tweet_id, :created_at, :updated_at)"
+    insdb.execute(sql, new_item)
+    insdb.close
+  end
 end
+
